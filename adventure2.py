5b3a2f75219f2146fa4f25ce14b5ee4c06a93fd1
laumitt <laumitt@nuevaschool.org>
2017-11-28 21:26:58 -0800

    cleaned up move/action but torch part isn't working

--- adventure2.py
+++ adventure2.py
@@ -1,5 +1,5 @@
 class Place:
-    def __init__(self, locnorth, loceast, locsouth, locwest, locup, locdown):
+    def __init__(self, locnorth, loceast, locsouth, locwest, locup, locdown, player):
         self.NORTH = locnorth # so that each place can define its own
         self.EAST = loceast # surroundings but run off one class
         self.SOUTH = locsouth
@@ -10,6 +10,7 @@ class Place:
         self.blurb = None
     def description(self): # to describe each location and by extension
         print(self.blurb) # give the player possible directions
+        print("You have " + str(player.inventory)) # show the player what they have
     def dir_check(self): # debug only
         print("You can go " +
               "\n" + "North = " + str(self.NORTH) +
@@ -21,7 +22,7 @@ class Place:
     def change_location(self, player): # Place knows where Player ends up if
         if player.movement == 1: # they move a direction
             player.location = self.NORTH
-        elif player.movement == 2: # the movement cant break until later
+        elif player.movement == 2: # the movement can't break until later
             player.location = self.EAST  # and only if self.DIR is None
         elif player.movement == 3:
             player.location = self.SOUTH
@@ -40,127 +41,80 @@ class Player:
         self.movement = None
         self.location = None
         self.inventory = []
-    def move_input(self): # Player knows where to move
-        move = input()
-        if move == "go north":
-            self.movement = 1
-        elif move == "go east":
-            self.movement = 2
-        elif move == "go south":
-            self.movement = 3
-        elif move == "go west":
-            self.movement = 4
-        elif move == "go up":
-            self.movement = 5
-        elif move == "go down":
-            self.movement = 6
-        elif move == "stop": # to break the loop
-            self.movement = 0 # special symbol for breaking the loop
-        elif move == "pick up torch":
-            self.inventory.append("torch")
-            print("You gingerly pick up the torch and wonder how it got there.")
-            move = input() # can this be simplified?
-            if move == "go north":
-                self.movement = 1
-            elif move == "go east":
-                self.movement = 2
-            elif move == "go south":
-                self.movement = 3
-            elif move == "go west":
-                self.movement = 4
-            elif move == "go up":
-                self.movement = 5
-            elif move == "go down":
-                self.movement = 6
-            elif move == "stop": # to break the loop
-                self.movement = 0 # special symbol for breaking the loop
-            elif move == "light torch":
-                print("You try to light the torch but realize you forgot a lighter.")
-                move = input()
-                if move == "go north":
+    def action_input(self): # Player knows where to move
+        while True:
+            action = input()
+            if str(action) == "go " + "north" or "east" or "south" or "west" or "up" or "down":
+                if action == "go north":
                     self.movement = 1
-                elif move == "go east":
+                    break
+                elif action == "go east":
                     self.movement = 2
-                elif move == "go south":
+                    break
+                elif action == "go south":
                     self.movement = 3
-                elif move == "go west":
+                    break
+                elif action == "go west":
                     self.movement = 4
-                elif move == "go up":
+                    break
+                elif action == "go up":
                     self.movement = 5
-                elif move == "go down":
+                    break
+                elif action == "go down":
                     self.movement = 6
-                elif move == "stop": # to break the loop
-                    self.movement = 0 # special symbol for breaking the loop
-        elif move == "light torch":
-            print("You try to light the torch but realize you forgot a lighter.")
-            move = input()
-            if move == "go north":
-                self.movement = 1
-            elif move == "go east":
-                self.movement = 2
-            elif move == "go south":
-                self.movement = 3
-            elif move == "go west":
-                self.movement = 4
-            elif move == "go up":
-                self.movement = 5
-            elif move == "go down":
-                self.movement = 6
-            elif move == "stop": # to break the loop
+                    break
+            elif str(action) == "stop": # to break the loop
                 self.movement = 0 # special symbol for breaking the loop
-        else:
-            print("You realize you are spouting gibberish into thin air.")
-
+                break
+            elif str(action) == "pick up torch": # this part isn't working
+                self.inventory.append("torch")
+                print("You gingerly pick up the torch and wonder how it got there.")
+                print("You have " + str(self.inventory))
+            elif str(action) == "light torch" and self.inventory.count("torch") >= 1:
+                print("You try to light the torch but realize you forgot a lighter.")
 
 if __name__ == "__main__":
-    field = Place("cave", "river one", None, "forest one", None, None)
-    field.blurb = "You stand in a large field with a forest to the west, a river to the east, and a cave to the north."
-    cave = Place(None, None, "field", None, "ledge", None)
-    cave.blurb = "You stand at the mouth of a dark cave. There is an unlit torch on the ground. It looks like you can climb to a ledge above."
-    riverone = Place(None, None, None, "field", None, None)
-    riverone.blurb = "The river is burbling happily. You can't see anything interesting but the field to the west."
-    forestone = Place(None, "field", None, None, None, None)
-    forestone.blurb = "The forest is peaceful but empty. You can't see anything interesting but the field to the east."
-    ledge = Place(None, None, None, None, None, "cave")
-    ledge.blurb = "The ledge is dark and empty. You can't see anything interesting but the cave below"
     player = Player()
+    field = Place("cave", "river one", None, "forest one", None, None, player)
+    field.blurb = "You stand in a large field with a forest to the west, a river to the east, and a cave to the north."
+    cave = Place(None, None, "field", None, "ledge", None, player)
+    cave.blurb = "You stand at the mouth of a dark cave. There is an unlit torch on the ground. It looks like you can climb to a ledge above."
+    riverone = Place(None, None, None, "field", None, None, player)
+    riverone.blurb = "The river is burbling happily. You can't see anything interesting but the field to the west."
+    forestone = Place(None, "field", None, None, None, None, player)
+    forestone.blurb = "The forest is peaceful but empty. You can't see anything interesting but the field to the east."
+    ledge = Place(None, None, None, None, None, "cave", player)
+    ledge.blurb = "The ledge is dark and empty. You can't see anything interesting but the cave below."
     print("Movement commands are go + north/south/east/west/up/down. All lowercase please. To exit, type stop." + '\n')
     player.location = field # start in field but stay in while loop later
     field.description() # would also put inventory here later
-    print(player.inventory)
-    player.move_input()
+    player.action_input()
     field.change_location(player)
     while True:
         if player.location == "cave":
             cave.description()
             #cave.dir_check()
-            player.move_input()
+            player.action_input()
             cave.change_location(player)
         elif player.location == "ledge":
             ledge.description()
             #ledge.dir_check()
-            player.move_input()
+            player.action_input()
             ledge.change_location(player)
         elif player.location == "river one":
             riverone.description()
             #riverone.dir_check()
-            player.move_input()
+            player.action_input()
             riverone.change_location(player)
         elif player.location == "forest one":
             forestone.description()
             #forestone.dir_check()
-            player.move_input()
+            player.action_input()
             forestone.change_location(player)
         elif player.location == "field":
             field.description()
             #field.dir_check()
-            player.move_input()
-            field.change_location(player)
-        elif player.location == None:
-            print("You realize you have been spouting gibberish into thin air.")
-            player.location = field # reset to field
-            field.description()
-            player.move_input()
+            player.action_input()
             field.change_location(player)
         elif player.location == "stop":
             break # because breaks can't be outside of loops
